db92515193b4efd2955032e469023164fea28ca1
NoraAmer <noraamer2015@gmail.com>
2016-04-10 14:01:21 +0200

    10-4-2016

--- app/src/main/java/com/example/nora/bubblestores/MainActivity.java
+++ app/src/main/java/com/example/nora/bubblestores/MainActivity.java
@@ -1,23 +1,135 @@
 package com.example.nora.bubblestores;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    public ActionBar supportActionBar;
+
+    NavigationView navigationView;
+    TextView shopNameNav;
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    ShopProfileFragment shopProfileFragment;
+    LoginFragment loginFragment;
+    RegistrationFragment registrationFragment;
+    AddItemFragment addItemFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureToolbar();
+        configureNavigationView();
+        configureDrawer();
+
     }
 
-    public void login(View view) {
-        startActivity(new Intent(this, LoginActivity.class));
+    private void configureToolbar() {
+        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create Navigation drawer and inlfate layout
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        // Adding menu icon to Toolbar
+        supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
-    public void register(View view) {
-        startActivity(new Intent(this,RegistrationActivity.class));
+    private void configureNavigationView() {
+        // Set behavior of Navigation drawer
+        assert navigationView != null;
+        View header = navigationView.getHeaderView(0);
+        shopNameNav = (TextView) header.findViewById(R.id.nameNavText);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    // This method will trigger on item Click of navigation menu
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // Set item in checked state
+                        menuItem.setChecked(true);
+
+                        if (menuItem.getItemId() == R.id.navProfileBTN) {
+                            fragmentManager = getSupportFragmentManager();
+                            fragmentTransaction = fragmentManager.beginTransaction();
+                            shopProfileFragment = new ShopProfileFragment();
+                            fragmentTransaction.replace(R.id.fragment_main, shopProfileFragment);
+                            fragmentTransaction.commit();
+                            mDrawerLayout.closeDrawers();
+                            return true;
+                        }
+                        if (menuItem.getItemId() == R.id.navLoginBTN) {
+                            fragmentManager = getSupportFragmentManager();
+                            fragmentTransaction = fragmentManager.beginTransaction();
+                            loginFragment = new LoginFragment();
+                            fragmentTransaction.replace(R.id.fragment_main, loginFragment);
+                            fragmentTransaction.commit();
+                            mDrawerLayout.closeDrawers();
+                            return true;
+                        }
+                        if (menuItem.getItemId() == R.id.navRegistrationBTN) {
+                            fragmentManager = getSupportFragmentManager();
+                            fragmentTransaction = fragmentManager.beginTransaction();
+                            registrationFragment = new RegistrationFragment();
+                            fragmentTransaction.replace(R.id.fragment_main, registrationFragment);
+                            fragmentTransaction.commit();
+                            mDrawerLayout.closeDrawers();
+                            return true;
+                        }
+
+                        if (menuItem.getItemId() == R.id.navAddItemBTN) {
+                            fragmentManager = getSupportFragmentManager();
+                            fragmentTransaction = fragmentManager.beginTransaction();
+                            addItemFragment = new AddItemFragment();
+                            fragmentTransaction.replace(R.id.fragment_main, addItemFragment);
+                            fragmentTransaction.commit();
+                            mDrawerLayout.closeDrawers();
+                            return true;
+                        }
+                        // Closing drawer on item click
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+    private void configureDrawer() {
+        // Configure drawer
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_closed) {
+
+            public void onDrawerClosed(View view) {
+                supportInvalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 }
