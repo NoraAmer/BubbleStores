fdcbaa924adb2ee6de81b21cab49e0fbc8384ce6
Mohamed Arafa <ghostarafa@gmail.com>
2016-04-11 14:58:44 +0200

    Registration

--- app/src/main/java/com/example/nora/bubblestores/RegisterShopFragment.java
+++ app/src/main/java/com/example/nora/bubblestores/RegisterShopFragment.java
@@ -2,6 +2,7 @@ package com.example.nora.bubblestores;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,7 +13,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.CursorLoader;
 import android.view.LayoutInflater;
@@ -25,22 +25,18 @@ import android.widget.Spinner;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-/**
- * Created by Nezar Saleh on 4/10/2016.
- */
 public class RegisterShopFragment extends Fragment {
 
-    NavigationView navigationView;
     private final int SELECT_FILE = 1;
     private final int REQUEST_CAMERA = 0;
     Spinner dropdown;
     Button add_Profile_Photo, add_Cover_Photo;
     ImageView Profile_Photo_Im, Cover_Photo_Im;
     int GlobalCode = 0;
+    private Core core;
 
 
     @Override
@@ -60,7 +56,7 @@ public class RegisterShopFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         dropdown = (Spinner) view.findViewById(R.id.spinner);
         String[] categories = new String[]{"Crafts", "Home", "Accessory", "Artwork", "Clothing"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, categories);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, categories);
         dropdown.setAdapter(adapter);
         add_Profile_Photo = (Button) view.findViewById(R.id.add_photo);
         add_Profile_Photo.setOnClickListener(new View.OnClickListener() {
@@ -71,7 +67,6 @@ public class RegisterShopFragment extends Fragment {
         });
         Profile_Photo_Im = (ImageView) view.findViewById(R.id.item_image);
 
-
         add_Cover_Photo = (Button) view.findViewById(R.id.add_cover_photo);
         add_Cover_Photo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,9 +75,13 @@ public class RegisterShopFragment extends Fragment {
             }
         });
         Cover_Photo_Im = (ImageView) view.findViewById(R.id.item_cover_image);
-
     }
 
+    @Override
+    public void onAttach(Context context) {
+        core = new Core(context);
+        super.onAttach(context);
+    }
 
     private void selectImage(int code) {
         GlobalCode = code;
@@ -119,34 +118,32 @@ public class RegisterShopFragment extends Fragment {
             if (requestCode == REQUEST_CAMERA) {
                 Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
                 ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+                if (thumbnail != null) {
+                    thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+                }
 
-                File destination = new File(Environment.getExternalStorageDirectory(),
-                        System.currentTimeMillis() + ".jpg");
+                File destination = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis() + ".jpg");
 
                 FileOutputStream fo;
                 try {
-                    destination.createNewFile();
                     fo = new FileOutputStream(destination);
                     fo.write(bytes.toByteArray());
                     fo.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 if (GlobalCode == 1) {
                     Profile_Photo_Im.setImageBitmap(thumbnail);
+                    core.uploadImage(destination.getPath());
                 } else if (GlobalCode == 2) {
                     Cover_Photo_Im.setImageBitmap(thumbnail);
+                    core.uploadImage(destination.getPath());
                 }
-
 
             } else if (requestCode == SELECT_FILE) {
                 Uri selectedImageUri = data.getData();
                 String[] projection = {MediaStore.MediaColumns.DATA};
-                CursorLoader cursorLoader = new CursorLoader(getContext(), selectedImageUri, projection, null, null,
-                        null);
+                CursorLoader cursorLoader = new CursorLoader(getContext(), selectedImageUri, projection, null, null, null);
                 Cursor cursor = cursorLoader.loadInBackground();
                 int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
                 cursor.moveToFirst();
@@ -166,14 +163,14 @@ public class RegisterShopFragment extends Fragment {
                 options.inJustDecodeBounds = false;
                 bm = BitmapFactory.decodeFile(selectedImagePath, options);
 
-
                 if (GlobalCode == 1) {
                     Profile_Photo_Im.setImageBitmap(bm);
+                    core.uploadImage(selectedImagePath);
                 } else if (GlobalCode == 2) {
                     Cover_Photo_Im.setImageBitmap(bm);
+                    core.uploadImage(selectedImagePath);
                 }
             }
         }
-
     }
 }
